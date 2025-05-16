224505ac72d70673abd481886da20b4a8c1f1f59
Norbert Żuchowski <norbertzuchowski@Norberts-MacBook-Air.local>
2025-05-16 17:26:50 +0200

    Dodano obsługę PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), 
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Aplikacja Finansowa',
+        short_name: 'Budżet',
+        start_url: '/app',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#7c3aed',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+        ],
+      },
+    })
+  ],
 })
